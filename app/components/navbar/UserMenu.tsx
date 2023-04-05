e661f534bad2af56fadedc297fb6bc38b885bdcd
Harrieson <harriemburu@gmail.com>
2023-04-05 22:34:01 +0300

    Modals

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,16 @@
 'use client'
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from '../Avatar'
+import { useState, useCallback } from 'react'
+import MenuItem from './MenuItem'
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toogleOpen = useCallback(
+      () => {
+        setIsOpen((value) => !value)
+      },[],
+    );
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -24,14 +33,26 @@ const UserMenu = () => {
                                 cursor-pointer transition
                                 border-neutral-200 hover:shadow-md
                                 "
-                                onClick={() => {}}
+                                onClick={toogleOpen}
                                 >
                                 <AiOutlineMenu />
-                </div>
-                <div className="hidden md:block">
-                    <Avatar />
+                                <div className="hidden md:block">
+                                <Avatar />
+                                </div>
                 </div>
             </div>
+            {isOpen && (
+                <div className="absolute rounded shadow-md 
+                                w-[40vw] md:w-3/4 bg-white
+                                overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem onClick={() => {}} label="Login" />
+                            <MenuItem onClick={() => {}} label="Sign Up" />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
